ffb2080aea1250d8ea67d2082e2c11a452d04af6
rafabill <rafasmour@gmail.com>
2024-03-28 20:07:01 +0200

    setting up traefik and unexpected TodoService error at NestJS

--- api/src/todo/todo.module.ts
+++ api/src/todo/todo.module.ts
@@ -5,11 +5,11 @@ import { TodoController } from "./todo.controller";
 import { TodoService } from "./todo.service";
 
 
-
 @Module({
     imports: [MongooseModule.forFeature([{name: 'todo', schema: todoSchema}])],
     controllers: [TodoController],
     providers: [TodoService],
+    exports: [TodoService]
 })
 export class TodoModule{
     
