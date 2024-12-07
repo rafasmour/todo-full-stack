14540b609c0a7242c4ea0442736ac3373f9ae60f
rafasmour <rafasmour@gmail.com>
2024-12-07 10:23:40 +0200

    fixed user initialization on mongo

--- data/init.sh
+++ data/init.sh
@@ -1,14 +1,25 @@
-# init.sh
+#!/bin/bash
 set -e
-mongosh -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD <<EOF
-use $MONGO_INITDB_DATABASE
+
+# Load required environment variables
+export $(grep -E '^(DATABASE_ROOT_USER|DATABASE_ROOT_PASS|DATABASE_ROOT_DATABASE|MONGODB_USER|MONGODB_PASS|MONGODB_DB)=' /docker-entrypoint-initdb.d/.env)
+
+# Create the root user in the admin database
+mongosh <<EOF
 use admin
 db.createUser({
+  user: '$DATABASE_ROOT_USER',
+  pwd: '$DATABASE_ROOT_PASS',
+  roles: [ { role: "root", db: "admin" } ]
+})
+EOF
+
+# Create the application user in the target database
+mongosh <<EOF
+use $DATABASE_DB
+db.createUser({
   user: '$MONGODB_USER',
-  pwd:  '$MONGODB_PASS',
-  roles: [{
-    role: 'readWrite',
-    db: '$MONGODB_DB'
-  }]
+  pwd: '$MONGODB_PASS',
+  roles: [ { role: "readWrite", db: "$DATABASE_DB" } ]
 })
 EOF
